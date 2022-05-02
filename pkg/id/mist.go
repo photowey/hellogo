5c8be8f7b85fb12f569eed18031ceaa476b51187
photowey <photowey@gmail.com>
2022-05-02 12:46:54 +0800

    feat: 1.add id generator

---
+++ pkg/id/mist.go
@@ -0,0 +1,36 @@
+package id
+
+import (
+	`crypto/rand`
+	`math/big`
+	`sync`
+)
+
+const saltBit = uint(8)               // 随机因子二进制位数
+const saltShift = uint(8)             // 随机因子移位数
+const incrShift = saltBit + saltShift // 自增数移位数
+
+type Mist struct {
+	sync.Mutex       // 互斥锁
+	incr       int64 // 自增数
+	saltA      int64 // 随机因子一
+	saltB      int64 // 随机因子二
+}
+
+func NewMist() *Mist {
+	mist := Mist{incr: 1}
+	return &mist
+}
+
+func (mt *Mist) Generate() int64 {
+	mt.Lock()
+	defer mt.Unlock()
+	mt.incr++
+	randA, _ := rand.Int(rand.Reader, big.NewInt(255))
+	mt.saltA = randA.Int64()
+	randB, _ := rand.Int(rand.Reader, big.NewInt(255))
+	mt.saltB = randB.Int64()
+	mist := int64((mt.incr << incrShift) | (mt.saltA << saltShift) | mt.saltB)
+
+	return mist
+}
---
+++ pkg/id/snowflake.go
@@ -0,0 +1,63 @@
+package id
+
+import (
+	`errors`
+	`sync`
+	`time`
+)
+
+const (
+	twepoch        = int64(1483228800000)             // 开始时间截 (2017-01-01)
+	workerIdBits   = uint(10)                         // 机器id所占的位数
+	sequenceBits   = uint(12)                         // 序列所占的位数
+	workerIdMax    = int64(-1 ^ (-1 << workerIdBits)) // 支持的最大机器id数量
+	sequenceMask   = int64(-1 ^ (-1 << sequenceBits)) //
+	workerIdShift  = sequenceBits                     // 机器id左移位数
+	timestampShift = sequenceBits + workerIdBits      // 时间戳左移位数
+)
+
+type Snowflake struct {
+	sync.Mutex
+	timestamp int64
+	workerId  int64
+	sequence  int64
+}
+
+func NewSnowflake(workerId int64) (*Snowflake, error) {
+
+	if workerId < 0 || workerId > workerIdMax {
+		return nil, errors.New("workerId must be between 0 and 1023")
+	}
+
+	return &Snowflake{
+		timestamp: 0,
+		workerId:  workerId,
+		sequence:  0,
+	}, nil
+}
+
+func (sf *Snowflake) Generate() int64 {
+
+	sf.Lock()
+	defer sf.Unlock()
+
+	now := time.Now().UnixNano() / 1000000
+
+	if sf.timestamp == now {
+		sf.sequence = (sf.sequence + 1) & sequenceMask
+
+		if sf.sequence == 0 {
+			for now <= sf.timestamp {
+				now = time.Now().UnixNano() / 1000000
+			}
+		}
+	} else {
+		sf.sequence = 0
+	}
+
+	sf.timestamp = now
+
+	nextId := int64((now-twepoch)<<timestampShift | (sf.workerId << workerIdShift) | (sf.sequence))
+
+	return nextId
+}
