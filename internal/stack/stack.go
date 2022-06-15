9be74a039fd63d5fc3febd2a54d4f41d14e211d3
photowey <photowey@gmail.com>
2022-06-16 02:28:02 +0800

    feat: 1.add stack

---
+++ internal/stack/stack.go
@@ -0,0 +1,83 @@
+package stack
+
+const (
+	EmptySlice = 0
+)
+
+type Element any
+
+type Stack interface {
+	Length() int
+	IsEmpty() bool
+	Push(element Element)
+	PushList(elements ...Element)
+	Pop() Element
+	Peek() Element
+	Clear()
+}
+
+type stack struct {
+	list []Element
+}
+
+func NewStack() Stack {
+	return &stack{list: make([]Element, 0)}
+}
+
+func (st *stack) Length() int {
+	return len(st.list)
+}
+
+func (st *stack) IsEmpty() bool {
+	return st.Length() == EmptySlice
+}
+
+func (st *stack) Push(element Element) {
+	st.list = append(st.list, element)
+}
+
+func (st *stack) PushList(elements ...Element) {
+	st.list = append(st.list, elements...)
+}
+
+func (st *stack) Pop() Element {
+	if st.IsEmpty() {
+		return nil
+	} else {
+		top := st.list[len(st.list)-1]
+		st.list = st.list[:len(st.list)-1]
+
+		return top
+	}
+}
+
+func (st *stack) Peek() Element {
+	if st.IsEmpty() {
+		return nil
+	} else {
+		return st.list[len(st.list)-1]
+	}
+}
+
+func (st *stack) Clear() {
+	if len(st.list) == 0 {
+		return
+	}
+	for i := 0; i < st.Length(); i++ {
+		st.list[i] = nil
+	}
+	st.list = make([]Element, 0)
+}
+
+func ToElement(element any) Element {
+	return Element(element)
+}
+
+func ToElements(elements ...any) []Element {
+	elementz := make([]Element, 0, len(elements))
+	for _, element := range elements {
+		elementz = append(elementz, element)
+	}
+
+	return elementz
+}
