868e3a6a1f125c7b8a61cd5302a12de91a48d735
photowey <photowey@gmail.com>
2022-08-03 01:35:52 +0800

    feat: 1.update hicooflake

--- pkg/hicooflake/hicooflake.go
+++ pkg/hicooflake/hicooflake.go
@@ -14,40 +14,21 @@ const (
 	incrShift = saltBit + factorBit
 )
 
-const (
-	defaultBase = uint64(1641063845000) // 2022-01-02 03:04:05
-)
-
 type Hicooflake struct {
 	sync.Mutex
-	incrBase uint64
 }
 
-func NewHicooflake(base ...uint64) *Hicooflake {
-	incrBase := defaultBase
-	switch len(base) {
-	case 1:
-		incrBase = base[0]
-	}
-
-	return &Hicooflake{
-		incrBase: incrBase,
-	}
-}
-
-func NowHicooflake() *Hicooflake {
-	now := uint64(time.Now().UnixNano() / 1e6)
-
-	return NewHicooflake(now)
+func NewHicooflake() *Hicooflake {
+	return &Hicooflake{}
 }
 
 func (hf *Hicooflake) NextId() uint64 {
 	hf.Lock()
 	defer hf.Unlock()
-	hf.incrBase++
+	now := uint64(time.Now().UnixNano() / 1e6)
 	salt := emit(1 << 8)
 	factor := emit(1 << 8)
-	nextId := hf.incrBase<<incrShift | uint64(salt<<(saltBit<<1)) | uint64(factor)
+	nextId := now<<incrShift | uint64(salt<<(saltBit<<1)) | uint64(factor)
 
 	return nextId
 }
--- pkg/hicooflake/hicooflake_test.go
+++ pkg/hicooflake/hicooflake_test.go
@@ -15,7 +15,7 @@ func TestHicooflake_NextId(t *testing.T) {
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			hf := NowHicooflake()
+			hf := NewHicooflake()
 			if got := hf.NextId(); got <= tt.want {
 				t.Errorf("NextId() = %v, want %v", got, tt.want)
 			}
