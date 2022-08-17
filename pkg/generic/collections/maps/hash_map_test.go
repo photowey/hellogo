030b711dafe53e2cb8b5f5a68e1df8fc520f6914
photowey <photowey@gmail.com>
2022-08-18 01:27:08 +0800

    feat: 1.add Get & Put ut

--- pkg/generic/collections/maps/hash_map_test.go
+++ pkg/generic/collections/maps/hash_map_test.go
@@ -1,6 +1,7 @@
 package maps
 
 import (
+	"reflect"
 	"testing"
 )
 
@@ -198,6 +199,107 @@ func TestHashMap_ContainsValue(t *testing.T) {
 			if got := hm.ContainsValue(tt.args.v); got != tt.want {
 				t.Errorf("ContainsValue() = %v, want %v", got, tt.want)
 			}
+		})
+	}
+}
+
+func TestHashMap_Get(t *testing.T) {
+	type fields[K comparable, V any] struct {
+		ctx map[K]V
+	}
+
+	type args[K comparable] struct {
+		k K
+	}
+
+	type Test struct {
+		name   string
+		fields fields[string, int64]
+		args   args[string]
+		want   int64
+		ok     bool
+	}
+	tests := []Test{
+		{
+			name: "Test HashMap#Get()-true",
+			fields: fields[string, int64]{
+				ctx: map[string]int64{
+					"1": 1,
+					"2": 2,
+				},
+			},
+			args: args[string]{
+				"2",
+			},
+			want: 2,
+			ok:   true,
+		},
+		{
+			name: "Test HashMap#Get()-false",
+			fields: fields[string, int64]{
+				ctx: map[string]int64{
+					"1": 1,
+					"2": 2,
+				},
+			},
+			args: args[string]{
+				"0",
+			},
+			want: 0,
+			ok:   false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			hm := &HashMap[string, int64]{
+				ctx: tt.fields.ctx,
+			}
+			got, got1 := hm.Get(tt.args.k)
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("Get() got = %v, want %v", got, tt.want)
+			}
+			if got1 != tt.ok {
+				t.Errorf("Get() got1 = %v, want %v", got1, tt.ok)
+			}
+		})
+	}
+}
+
+func TestHashMap_Put(t *testing.T) {
+	type fields[K comparable, V any] struct {
+		ctx map[K]V
+	}
+	type args[K comparable, V any] struct {
+		k K
+		v V
+	}
+	type Test struct {
+		name   string
+		fields fields[string, int64]
+		args   args[string, int64]
+	}
+
+	tests := []Test{
+		{
+			name: "Test HashMap#Put()",
+			fields: fields[string, int64]{
+				ctx: map[string]int64{
+					"1": 1,
+					"2": 2,
+				},
+			},
+			args: args[string, int64]{
+				k: "3",
+				v: 3,
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			hm := &HashMap[string, int64]{
+				ctx: tt.fields.ctx,
+			}
+			hm.Put(tt.args.k, tt.args.v)
 		})
 	}
 }
